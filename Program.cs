45e02bb76aa5926febfffad5cf0fffb29d784bb7
karkimilan <Milankarki723@gmail.com>
2022-11-27 11:51:12 +0545

    multiple object creationn

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@ public class Car
   string color = "red"; 
     public static void TestMethod()
     {
-        Car myObj = new Car(); 	
-		Console.WriteLine(myObj.color);
+        Car myObj1 = new Car(); 
+		Car myObj2 = new Car(); 
+		Console.WriteLine(myObj1.color); 
+		Console.WriteLine(myObj2.color); 
     }
 }

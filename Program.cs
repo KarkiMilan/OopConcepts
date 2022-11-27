561f4ddbad164e354a00f03196332d637269d123
karkimilan <Milankarki723@gmail.com>
2022-11-27 11:47:54 +0545

    Class Creation

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Car.TestMethod();
+public class Car
+{
+  string color = "red"; 
+    public static void TestMethod()
+    {
+        Car myObj = new Car(); 	
+		Console.WriteLine(myObj.color);
+    }
+}

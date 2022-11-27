236b52532a5fd400a27a1ff4e58b7695588444d0
karkimilan <Milankarki723@gmail.com>
2022-11-27 12:45:38 +0545

    Setting value to constructor

--- Program.cs
+++ Program.cs
@@ -1,14 +1,20 @@
 ﻿Car.Suv();
 class Car
 {
-  string model;
-    public Car()
+  public string model;
+  public string color;
+  public int year;
+    public Car(string modelName, string modelColor, int modelYear)
     {
-        model = "Mustang";
+        model = modelName;
+        color = modelColor;
+        year = modelYear;
     }
     public static void Suv()
     {
-        Car Ford = new Car();
-        Console.WriteLine(Ford.model);       
+        Car Ford = new Car("Mustang", "Red", 1969);
+        Car Opel = new Car("Astra", "White", 2005);
+        Console.WriteLine(Ford.model);
+        Console.WriteLine(Opel.model);     
     }
 }

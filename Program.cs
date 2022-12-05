75b9b9d43613c92d60a9ece10539ffd6fd358c66
karkimilan <Milankarki723@gmail.com>
2022-12-05 19:20:59 +0545

    Static Class

--- Program.cs
+++ Program.cs
@@ -1,21 +1,61 @@
-﻿using System;
-
-namespace ThisReference
+﻿public static class TemperatureConverter
 {
-    public class ThisRef
+    public static double CelsiusToFahrenheit(string temperatureCelsius)
     {
-        public class Thissser
+        // Convert argument to double for calculations.
+        double celsius = Double.Parse(temperatureCelsius);
+
+        // Convert Celsius to Fahrenheit.
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        return fahrenheit;
+    }
+
+    public static double FahrenheitToCelsius(string temperatureFahrenheit)
+    {
+        // Convert argument to double for calculations.
+        double fahrenheit = Double.Parse(temperatureFahrenheit);
+
+        // Convert Fahrenheit to Celsius.
+        double celsius = (fahrenheit - 32) * 5 / 9;
+
+        return celsius;
+    }
+}
+
+class TestTemperatureConverter
+{
+    static void Main()
+    {
+        Console.WriteLine("Please select the convertor direction");
+        Console.WriteLine("1. From Celsius to Fahrenheit.");
+        Console.WriteLine("2. From Fahrenheit to Celsius.");
+        Console.Write(":");
+
+        string? selection = Console.ReadLine();
+        double F, C = 0;
+
+        switch (selection)
         {
-            public string whatever;
-            public Thissser(string whatever) {
-                this.whatever = whatever;
-                Console.WriteLine($"The constructor was called with argument :: {whatever}");
-            }
+            case "1":
+                Console.Write("Please enter the Celsius temperature: ");
+                F = TemperatureConverter.CelsiusToFahrenheit(Console.ReadLine() ?? "0");
+                Console.WriteLine("Temperature in Fahrenheit: {0:F2}", F);
+                break;
+
+            case "2":
+                Console.Write("Please enter the Fahrenheit temperature: ");
+                C = TemperatureConverter.FahrenheitToCelsius(Console.ReadLine() ?? "0");
+                Console.WriteLine("Temperature in Celsius: {0:F2}", C);
+                break;
+
+            default:
+                Console.WriteLine("Please select a convertor.");
+                break;
         }
-        static void Main(String[] args)
-        {
-            Thissser Demo = new Thissser("Well this is it!");
-            Console.Read();
-        }
+
+        // Keep the console window open in debug mode.
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
     }
 }

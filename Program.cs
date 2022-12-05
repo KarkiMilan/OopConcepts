8ffc3bc19ab9e81d5b841a954df5ff7953d0c3d8
karkimilan <Milankarki723@gmail.com>
2022-12-05 19:07:49 +0545

    sample class and instantiate

--- Program.cs
+++ Program.cs
@@ -1,23 +1,22 @@
 ﻿using System;
-class SampleCollection<T>
+namespace ThisReference
 {
-   // Declare an array to store the data elements.
-   private T[] arr = new T[100];
-
-   // Define the indexer to allow client code to use [] notation.
-   public T this[int i]
-   {
-      get { return arr[i]; }
-      set { arr[i] = value; }
-   }
-}
-
-class Program
-{
-   static void Main()
-   {
-      var stringCollection = new SampleCollection<string>();
-      stringCollection[0] = "Hello, World";
-      Console.WriteLine(stringCollection[0]);
-   }
+    public class ThisRef
+    {
+        public class SampleClass {
+            public string SampleProperty { get; set; }
+            public string SampleMethod(string message)
+            {
+                return $"This is the message: {message}";
+            }
+        }
+        static void Main(String[] args)
+        {
+            SampleClass test = new SampleClass();
+            test.SampleProperty = "Pluralsight";
+            Console.WriteLine($"This is the value of the SampleProperty: {test.SampleProperty}");
+            Console.WriteLine($"This is the message from the SampleMethod: {test.SampleMethod("This is cool")}");
+            Console.Read();
+        }
+    }
 }
